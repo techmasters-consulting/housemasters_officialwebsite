39435298c3973e832a07cd17b1e78b31d7ed3478
thecomputermanonline <olalekanarowoselu@gmail.com>
2021-07-27 01:15:46 +0400

    https fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,32 +11,32 @@
     <meta name="author" content="Housemasters.mu">
     <title>Housemasters | Real Estate | Services</title>
     <!-- Bootstrap v4.3.1 CSS via CDN -->
-    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
 
 
     <!-- Plugins required on all pages NOTE: Additional non-required plugins are loaded ondemand -->
 
 
-    <link href="{{asset('assets/css/theme-style.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('assets/css/theme-style.min.css')}}" rel="stylesheet">
 
     <!-- Bright green colour scheme -->
     <link href="" rel="stylesheet">
-    <link href="{{asset('assets/css/theme-style.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('assets/css/theme-style.min.css')}}" rel="stylesheet">
     <!--Your custom colour override-->
     <link href="#" id="colour-scheme" rel="stylesheet">
 
     <!-- Your custom override -->
-    <link href="{{asset('assets/css/custom-style.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('assets/css/custom-style.css')}}" rel="stylesheet">
 
 
 
     <!-- Optional: ICON SETS -->
     <!-- Iconset: Font Awesome 5.0.13 via CDN -->
     <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
-    <!-- Iconset: flag icons - http://lipis.github.io/flag-icon-css/ -->
+    <!-- Iconset: flag icons - https://lipis.github.io/flag-icon-css/ -->
     <link href="assets/plugins/flag-icon-css/css/flag-icon.min.css" rel="stylesheet">
-    <!-- Iconset: ionicons - http://ionicons.com/ -->
-    <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
+    <!-- Iconset: ionicons - https://ionicons.com/ -->
+    <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
     <!-- Iconset: Linearicons - https://linearicons.com/free -->
     <link href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css" rel="stylesheet">
     <!-- Iconset: Lineawesome - https://icons8.com/articles/line-awesome -->
@@ -44,17 +44,17 @@
 
 
     <!-- Le fav and touch icons - @todo: fill with your icons or remove, try https://realfavicongenerator.net -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/favicons/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/favicons/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/favicons/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{asset('assets/favicons/manifest.json')}}">
-    <link rel="shortcut icon" href="{{asset('assets/favicons/favicon.ico')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{secure_asset('assets/favicons/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{secure_asset('assets/favicons/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{secure_asset('assets/favicons/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{secure_asset('assets/favicons/manifest.json')}}">
+    <link rel="shortcut icon" href="{{secure_asset('assets/favicons/favicon.ico')}}">
     <!-- Google fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Rambla:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Calligraffitti' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto:100,400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Rambla:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Calligraffitti' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,400,700' rel='stylesheet' type='text/css'>
 
 
 
